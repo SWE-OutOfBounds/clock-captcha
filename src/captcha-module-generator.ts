0dea2dfe13aa0d8fbf0dd4f77f0bbd67da6b04b2
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-02-01 10:33:16 +0100

    ADD : module generation ad module dresser function

---
+++ src/captcha-module-generator.ts
@@ -0,0 +1,6 @@
+
+export function generateCaptcha{
+    let captchaContainer : HTMLElement = (document.getElementById('clock-captcha'));
+    //check if element exists
+    containerDressing(captchaContainer);
+}
---
+++ src/styles/containerDressing.ts
@@ -0,0 +1,4 @@
+export function containerDressing(container: HTMLElement) : void{
+    let styleSheet : string = "width: 250px; height";
+    container.setAttribute("style", styleSheet);
+}
