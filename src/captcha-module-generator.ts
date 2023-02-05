cdf80100cf4afc30564d66cc18b66039a42391e7
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-02-05 10:24:14 +0100

    ADD : input, time generation

--- src/captcha-module-generator.ts
+++ src/captcha-module-generator.ts
@@ -2,9 +2,13 @@
 import { Canvg } from "canvg";
 
 // Internal Modules
-import { canvasDressing, containerDressing } from "./stylist";
+import { canvasDressing, containerDressing, inputDressing } from "./stylist";
 
-function setClock(canvas: HTMLCanvasElement) {
+function setClock(canvas: HTMLCanvasElement, time:number) {
+    let hrRotation : number = (360/12) * (Math.trunc(time/3600)%12);
+    let minRotation : number = (360/60) * (Math.trunc(time/60)%60);
+    let secRotation : number = (360/60) * (time%60);
+    
     let v = null;
 
     const ctx = canvas ? canvas.getContext('2d') : null;
@@ -18,15 +22,15 @@ function setClock(canvas: HTMLCanvasElement) {
         d="M300.5 94V61M506 300.5h32M300.5 506v33M94 300.5H60M411.3 107.8l7.9-13.8M493 190.2l13-7.4M492.1 411.4l16.5 9.5M411 492.3l8.9 15.3M189 492.3l-9.2 15.9M107.7 411L93 419.5M107.5 189.3l-17.1-9.9M188.1 108.2l-9-15.6" />
       <circle class="mid-circle" cx="300" cy="300" r="16.2" fill="black" />
     </g>
-    <g id="hour" style="transform-origin: 300px 300px; transition: transform 0.5s ease-in-out; transform: rotate(`+ Math.random() * 360 + `);">
+    <g id="hour" style="transform-origin: 300px 300px; transition: transform 0.5s ease-in-out; transform: rotate(`+ hrRotation + `);">
       <path class="hour-arm" d="M300.5 298V142" fill="none" fill-rule="evenodd" stroke="black" stroke-width="17" stroke-miterlimit="10" />
       <circle class="sizing-box" cx="300" cy="300" r="253.9" fill="none" />
     </g>
-    <g id="minute" style="transform-origin: 300px 300px; transition: transform 0.5s ease-in-out; transform: rotate(`+ Math.random() * 360 + `);">
+    <g id="minute" style="transform-origin: 300px 300px; transition: transform 0.5s ease-in-out; transform: rotate(`+ minRotation + `);">
       <path class="minute-arm" d="M300.5 298V67" fill="none" fill-rule="evenodd" stroke="black" stroke-width="11" stroke-miterlimit="10" />
       <circle class="sizing-box" cx="300" cy="300" r="253.9" fill="none" />
     </g>
-    <g id="second" style="transform-origin: 300px 300px; transition: transform 0.5s ease-in-out; transform: rotate(`+ Math.random() * 360 + `);">
+    <g id="second" style="transform-origin: 300px 300px; transition: transform 0.5s ease-in-out; transform: rotate(`+ secRotation + `);">
       <path class="second-arm" d="M300.5 350V55" fill="none" fill-rule="evenodd" stroke="black" stroke-width="4"  stroke-miterlimit="10" />
       <circle class="sizing-box" cx="300" cy="300" r="253.9" fill="none"/>
     </g>
@@ -40,14 +44,40 @@ function setClock(canvas: HTMLCanvasElement) {
     if (v) v.start();
 };
 
-export function generateCaptcha() {
+export function generateCaptcha(): number {
+    let time : number = Math.trunc(Math.random() * (2*24*60*60));
+
     let captchaContainer: HTMLElement = (document.getElementById('clock-captcha'));
     //check if element exists
+    containerDressing(captchaContainer);
 
     let canvas = document.createElement('canvas');
+    canvasDressing(canvas);
     captchaContainer.appendChild(canvas);
 
-    containerDressing(captchaContainer);
-    canvasDressing(canvas);
-    setClock(canvas);
+    let inputContainer = document.createElement('div');
+
+    let hoursInput = document.createElement('input');
+    hoursInput.setAttribute("type", "text");
+    hoursInput.setAttribute("maxlength", "2");
+    inputDressing(hoursInput);
+    inputContainer.appendChild(hoursInput);
+
+    let minutesInput = document.createElement('input');
+    minutesInput.setAttribute("type", "text");
+    minutesInput.setAttribute("maxlength", "2");
+    inputDressing(minutesInput);
+    inputContainer.appendChild(minutesInput);
+
+    let secondsInput = document.createElement('input');
+    secondsInput.setAttribute("type", "text");
+    secondsInput.setAttribute("maxlength", "2");
+    inputDressing(secondsInput);
+    inputContainer.appendChild(secondsInput);
+
+    captchaContainer.appendChild(inputContainer);
+
+    setClock(canvas, time);
+
+    return time;
 }
--- src/stylist.ts
+++ src/stylist.ts
@@ -3,6 +3,10 @@ export function canvasDressing(canvas: HTMLCanvasElement) : void{
     canvas.height = 100;
 }
 export function containerDressing(container: HTMLElement) : void{
-    let styleSheet : string = "width: 300px;height: 100px;background-color: #333;border-radius: 20px;border: 1px solid white;outline: 1px solid black;";
+    let styleSheet : string = "width: 300px;height: 100px;background-color: #333;border-radius: 20px;border: 1px solid white;outline: 1px solid black; display: flex; flex-flow: row no-wrap; justify-content: space-evenly; align-items:center";
+    container.setAttribute("style", styleSheet);
+}
+export function inputDressing(container: HTMLElement) : void{
+    let styleSheet : string = "width: 2ch; font-size:2rem; padding: 5px; backgroud-color: cyan; border: 1px solid black; border-radius:10px;";
     container.setAttribute("style", styleSheet);
 }
