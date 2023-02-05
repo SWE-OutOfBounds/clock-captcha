850a88d53e5a85b5bc5fd6915c0daf2254602fe6
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-02-05 08:25:37 +0100

    ADD : .gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayGoodbye = exports.sayHello = void 0;
-var hello_world_1 = require("./hello-world");
-Object.defineProperty(exports, "sayHello", { enumerable: true, get: function () { return hello_world_1.sayHello; } });
-Object.defineProperty(exports, "sayGoodbye", { enumerable: true, get: function () { return hello_world_1.sayGoodbye; } });
+exports.generateCaptcha = void 0;
+var captcha_module_generator_1 = require("./captcha-module-generator");
+Object.defineProperty(exports, "generateCaptcha", { enumerable: true, get: function () { return captcha_module_generator_1.generateCaptcha; } });
