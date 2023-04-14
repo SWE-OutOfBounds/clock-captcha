51af0f9153e971e05d0aeb50bc78a96c96e4fb8a
dipo-ThinkPad <jacopo.angeli@studenti.unipd.it>
2023-04-14 14:58:39 +0200

    ADD : DistortionDecoration class Added only distortionCurveGeneration function which is to use in draw()

---
+++ src/DistortionDecorator.ts
@@ -0,0 +1,44 @@
+import { ClockCAPTCHAInterface } from "./ClockCAPTCHAInterface";
+import { Decorator } from "./Decorator";
+
+export class DistortionDecorator extends Decorator {
+    constructor(component: ClockCAPTCHAInterface, distortionFactor: number) {
+        super(component);
+        //component() => component.draw()
+        this._distortionFactor = distortionFactor;
+        this.draw();
+    }
+
+    public draw(): void {
+        const ctx = this._component.getCanvas().getContext('2d');
+        const width = this._component.getCanvas().width;
+        const height = this._component.getCanvas().height;
+
+       
+    }
+
+    private distortionCurveGeneration(canvasWidth : number, canvasHeight : number) : Array<number>{
+        var curve: Array<number> = [Math.trunc(Math.random()*canvasWidth)];
+
+        //Scelgo randomicamente se salie o scendere
+        let direction: boolean = Math.random() < 0.5;
+        
+        while (canvasHeight > 0) {
+            //calcolo l'altezza della sezione della curva come massimo 60% e minimo 40% della larghezza disponibile
+            let amplitude = Math.trunc(Math.random() * (canvasWidth * 0.2) + (canvasWidth * 0.4)), flexPoint = Math.trunc(amplitude / 2);
+            if (amplitude % 2 == 0) amplitude++;
+        
+            while (amplitude > 0 && canvasHeight > 0) {
+                if (amplitude == flexPoint) direction = !direction;
+                else curve.push(direction == true ? curve[curve.length - 1] + 1 : curve[curve.length - 1] -1);
+                canvasHeight--;
+                amplitude--;
+            }
+        
+        }
+
+        return curve;
+    }
+
+    private _distortionFactor: number;
+}
