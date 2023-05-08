3b5ec2a4942bbf1d260419862442f630c584da7b
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-05-08 14:52:13 +0200

    CONFORMED TO DESIGN

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export { ClockCAPTCHA } from "./back-end/ClockCAPTCHA";
 export { ClockImageGenerator } from "./back-end/ClockImageGenerator";
 export { HTMLCanvasGenerator } from "./back-end/ClockImageGeneratorStrategy/strategies/HTMLCanvasStrategy/HTMLCanvasGenerator";
 export { ShapesDecorator } from "./back-end/ClockImageGeneratorStrategy/strategies/HTMLCanvasStrategy/ShapesDecorator";
+export { NoiseDecorator } from './back-end/ClockImageGeneratorStrategy/strategies/HTMLCanvasStrategy/NoiseDecorator';
