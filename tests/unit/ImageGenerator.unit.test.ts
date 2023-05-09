3b6de5395d9620a9133bf9094f12cb29b7ce8e8d
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-05-09 13:40:27 +0200

    SOME OTHER TESTS

---
+++ tests/unit/ImageGenerator.unit.test.ts
@@ -0,0 +1,34 @@
+import { ClockImageGenerator, HTMLCanvasGenerator } from "../../src/index";
+
+describe("ClockImageGenerator unit tests", () => {
+    let ImageGenerationStrategy = new HTMLCanvasGenerator();
+    let ImageGenerator = new ClockImageGenerator(ImageGenerationStrategy);
+
+    describe("Interfaccia", () => {
+
+        it("Fornisce il metodo generateImage()", () => {
+            expect(typeof ImageGenerator.generateImage).toBe('function');
+        })
+
+    })
+
+    describe("Funzionamento di base", () => {
+
+        it("Chiamata con parametri fuori range solleva eccezione", () => {
+            const outOfRangeParameters = () => {
+                ImageGenerator.generateImage(123, 123);
+            }
+            expect(outOfRangeParameters).toThrow(Error);
+        })
+
+        it("Chiamata con parametri corretti ritorna una stringa", () => {
+            expect(ImageGenerator.generateImage(10, 10)).toEqual(expect.any(String));
+        })
+
+        it("Stringa ritornata del giusto formato", ()=>{
+            expect(ImageGenerator.generateImage(10, 10).length).toBeGreaterThan(0);
+        })
+
+    })
+
+})
