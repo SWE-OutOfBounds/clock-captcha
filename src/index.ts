de00f4c10679881e61547673f89d68e5e7eab031
dipo-ThinkPad <jacopo.angeli@studenti.unipd.it>
2023-05-27 15:16:38 +0200

    BUG FIX

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 // Front-end exports
-export { ClockCAPTCHAView } from "./front-end/ClockCAPTCHA";
+export { ClockCAPTCHAView } from "./front-end/ClockCAPTCHAView";
 
 // Back-end exports
 export { ClockCAPTCHA } from "./back-end/ClockCAPTCHA";
 export { ClockImageGenerator } from "./back-end/ClockImageGenerator";
 export { HTMLCanvasGenerator } from "./back-end/ClockImageGeneratorStrategy/strategies/HTMLCanvasStrategy/HTMLCanvasGenerator";
 export { ShapesDecorator } from "./back-end/ClockImageGeneratorStrategy/strategies/HTMLCanvasStrategy/ShapesDecorator";
-export { NoiseDecorator } from './back-end/ClockImageGeneratorStrategy/strategies/HTMLCanvasStrategy/NoiseDecorator';
+export { NoiseDecorator } from "./back-end/ClockImageGeneratorStrategy/strategies/HTMLCanvasStrategy/NoiseDecorator";
